5c862179f07a85bceb35eadf36913e072b93b391
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-01-26 11:25:07 +0100

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { SeenIcon } from "./svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="dark"></div>;
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
