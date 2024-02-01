2e949b3ab75e6024e2bfdd167d3885c2e296a654
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-01 16:24:53 +0100

    Added some styling to Login and Signup pages and downloaded some dependencies

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,13 @@
+import SignupForm from "../components/auth/SignupForm";
+
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden text-white">
+      <div className="flex w-[1600px] mx-auto h-full">
+        <SignupForm />
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
