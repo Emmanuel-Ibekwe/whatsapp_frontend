23841eb248152d91a16114dd8e51362638164d26
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-15 13:54:32 +0100

    feat: created the chatSlice

---
+++ src/store/chatSlice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const CONVERSATION_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT}/conversation`;
+
+const initialState = {
+  status: "",
+  error: "",
+  conversations: [],
+  activeConversation: {},
+  notifications: []
+};
+
+export const getConversations = createAsyncThunk(
+  "conversations/all",
+  async (token, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(CONVERSATION_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return data;
+    } catch (error) {
+      rejectWithValue(error.response.data.message);
+    }
+  }
+);
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    setActiveConversation: (state, action) => {
+      state.activeConversation = action.payload;
+    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getConversations.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getConversations.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.conversations = action.payload;
+      })
+      .addCase(getConversations.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  }
+});
+
+export const { setActiveConversation } = chatSlice.actions;
+export default chatSlice.reducer;
