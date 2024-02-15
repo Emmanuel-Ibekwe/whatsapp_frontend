1023369a0f0c0f2d0b3ac6e5d3f132f7b94bb3b2
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-15 14:02:51 +0100

    style: Added the relative class to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import Sidebar from "../components/sidebar/Sidebar";
+import { useSelector, useDispatch } from "react-redux";
+import { getConversations } from "../store/chatSlice";
+
 function Home() {
+  const { user } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (user?.token) {
+      dispatch(getConversations(user.token));
+    }
+  }, [user, dispatch]);
+
   return (
-    <div>
-      <h1 className="text-5xl">Welcome to the Home page</h1>
+    <div className="relative min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      <div className="container min-h-screen flex">
+        <Sidebar />
+      </div>
     </div>
   );
 }
