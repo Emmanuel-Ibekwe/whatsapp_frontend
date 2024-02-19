5078ddb47de4bfc6ac2859bcdffbea933054e7f3
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-19 17:37:13 +0100

    feat: Created the WhatsappHome and Menu components

---
+++ src/components/sidebar/header/Menu.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from "react-redux";
+
+import { logout } from "../../../store/userSlice";
+
+export default function Menu() {
+  const dispatch = useDispatch();
+  return (
+    <div className="absolute right-1 z-50 dark:bg-dark_bg_4 dark:text-dark_text_1 shadow-md w-64 rounded-sm text-left border-b border-b-dark_bg_2">
+      <ul>
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+          <span>New Group</span>
+        </li>
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+          <span>New Community</span>
+        </li>
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+          <span>Starred Messages</span>
+        </li>
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+          <span>Select Chat</span>
+        </li>
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+          <span>Settings</span>
+        </li>
+        <li
+          className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
+          onClick={() => dispatch(logout())}
+        >
+          <span>Log out</span>
+        </li>
+
+        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3 border-t dark:border-t-dark_border_2">
+          <span>Get Whatsapp for Windows</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
