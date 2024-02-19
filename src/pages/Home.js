b40b2654d8da56fb3676eb09721214b486e62025
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-19 17:24:04 +0100

    chore: minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,12 @@ import { useEffect } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import { useSelector, useDispatch } from "react-redux";
 import { getConversations } from "../store/chatSlice";
+import WhatsappHome from "../components/Chat/WhatsappHome";
 
 function Home() {
   const { user } = useSelector(state => state.user);
+  const { activeConversation } = useSelector(state => state.chat);
+  console.log("activeConversation", activeConversation);
   const dispatch = useDispatch();
   useEffect(() => {
     if (user?.token) {
@@ -13,9 +16,10 @@ function Home() {
   }, [user, dispatch]);
 
   return (
-    <div className="relative min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
-      <div className="container min-h-screen flex">
+    <div className="relative h-screen dark:bg-dark_bg_1  justify-center overflow-hidden">
+      <div className=" h-screen flex items w-full">
         <Sidebar />
+        {activeConversation?._id ? "Home" : <WhatsappHome />}
       </div>
     </div>
   );
