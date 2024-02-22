3d2b475fbe882199fda092bc47210585c64412d2
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-22 14:05:18 +0100

    refactor: created the mapMessages function

---
+++ src/components/Chat/messages/ChatMessages.jsx
@@ -0,0 +1,45 @@
+import { useSelector } from "react-redux";
+import Message from "./Message";
+import { sameDay } from "../../../utils/date";
+
+export default function ChatMessages() {
+  const { messages } = useSelector(state => state.chat);
+  const { user } = useSelector(state => state.user);
+
+  function mapMessages(message, i, arr) {
+    let setCornerTriangle, showDate;
+    console.log("message_id", message._id);
+    if (i <= 0 || message.sender._id !== arr[i - 1]?.sender._id) {
+      setCornerTriangle = true;
+    } else {
+      setCornerTriangle = false;
+    }
+
+    const currentMessageDate = new Date(message.updatedAt);
+    const dateofMsgBeforeCurMsg = new Date(arr[i - 1]?.updatedAt);
+
+    if (i <= 0 || !sameDay(currentMessageDate, dateofMsgBeforeCurMsg)) {
+      showDate = true;
+    } else {
+      showDate = false;
+    }
+
+    return (
+      <Message
+        message={message}
+        key={message._id}
+        me={user._id === message.sender._id}
+        setCornerTriangle={setCornerTriangle}
+        showDate={showDate}
+      />
+    );
+  }
+
+  return (
+    <div className="w-full mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-cover bg-no-repeat">
+      <div className="w-full scrollbar overflow_scrollbar overflow-auto py-2 px-[6%]">
+        {messages && messages.map(mapMessages)}
+      </div>
+    </div>
+  );
+}
