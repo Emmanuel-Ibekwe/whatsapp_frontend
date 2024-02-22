626438fb49015fb9b62114df13ea5cf61cbc4bf5
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-22 13:59:14 +0100

    feat & refactor: Refactored the getDay function and created the sameDay and getMessageDate function

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,15 @@
 import moment from "moment";
 
+const getDay = (time, now, momentDate) => {
+  let days = time.split(" ")[0];
+  if (Number(days) < 7) {
+    return now.subtract(Number(days), "days").format("dddd");
+  } else {
+    console.log("DD/MM/YYYY", momentDate.format("DD/MM/YYYY"));
+    return momentDate.format("DD/MM/YYYY");
+  }
+};
+
 export const dateHandler = date => {
   //   let laggingTime = new Date(date);
   //   let currentTime = laggingTime.setHours(laggingTime.getHours() + 1);
@@ -7,15 +17,6 @@ export const dateHandler = date => {
   let momentDate = moment(date);
   let time = momentDate.fromNow(true);
   let dateByHourAndMinute = momentDate.format("LT");
-
-  const getDay = () => {
-    let days = time.split(" ")[0];
-    if (Number(days) < 7) {
-      return now.subtract(Number(days), "days").format("dddd");
-    } else {
-      return momentDate.format("DD/MM/YYYY");
-    }
-  };
 
   //   if (time === "a few seconds") {
   //     return "Now";
@@ -42,8 +43,36 @@ export const dateHandler = date => {
   }
 
   if (time.search("days") !== -1) {
-    return getDay();
+    return getDay(time, now, momentDate);
   }
 
-  return time;
+  return getDay(time, now, momentDate);
+};
+
+export function sameDay(date1, date2) {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  );
+}
+
+export const getMessageDate = date => {
+  let now = moment();
+  let momentDate = moment(date);
+  let time = momentDate.fromNow(true);
+
+  if (
+    time.search("hour") !== -1 ||
+    time.search("minute") !== -1 ||
+    time === "a few seconds" ||
+    time === "a day"
+  ) {
+    return "Yesterday".toUpperCase();
+  }
+
+  if (time.search("days") !== -1) {
+    return getDay(time, now, momentDate).toUpperCase();
+  }
+  return getDay(time, now, momentDate).toUpperCase();
 };
