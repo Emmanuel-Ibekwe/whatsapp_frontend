a0eca1c4705884068f5f1a79fb10e6371376c2dd
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-24 21:36:55 +0100

    feat: Added features to enabled emojis to be added to the input value

--- src/components/Chat/actions/EmojiPickerButton.jsx
+++ src/components/Chat/actions/EmojiPickerButton.jsx
@@ -1,11 +1,50 @@
+import { useState, useEffect } from "react";
 import { EmojiIcon } from "../../../svg";
+import EmojiPicker from "emoji-picker-react";
 
-export default function EmojiPickerButton() {
+export default function EmojiPickerButton({ textRef, onSetMessage, message }) {
+  const [showPicker, setShowPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState(0);
+
+  useEffect(() => {
+    textRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+
+  const handleEmoji = (emojiData, e) => {
+    const { emoji } = emojiData;
+    const ref = textRef.current;
+    ref.focus();
+    const start = message.substring(0, ref.selectionStart);
+    const end = message.substring(ref.selectionStart);
+    const newText = start + emoji + end;
+    onSetMessage(newText);
+    setCursorPosition(start.length + emoji.length);
+  };
+
   return (
     <li>
-      <button className="btn" type="button">
-        <EmojiIcon className="dark:fill-dark_svg_1" />
+      <button
+        className="btn"
+        type="button"
+        onClick={() => setShowPicker(prev => !prev)}
+      >
+        <EmojiIcon
+          className={`${
+            showPicker ? "dark:fill-dark_svg_2" : "dark:fill-dark_svg_1"
+          }`}
+        />
       </button>
+      {showPicker && (
+        <div>
+          <div
+            className="absolute bottom-0 w-full h-screen"
+            onClick={() => setShowPicker(false)}
+          ></div>
+          <div className="openEmojiAnimation absolute bottom-[70px] left-[50px] w-[375px]">
+            <EmojiPicker onEmojiClick={handleEmoji} theme="dark" />
+          </div>
+        </div>
+      )}
     </li>
   );
 }
