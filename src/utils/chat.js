ac045be9892d82ddcb7b2b7856c885a7690e7844
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-24 21:28:28 +0100

    feat: Created the sortInDescendingLatestMessageTime function

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,3 +1,12 @@
 export const getConversationId = (user, users) => {
   return users[0]._id === user._id ? users[1]._id : users[0]._id;
 };
+
+export const sortInDescendingLatestMessageTime = arr => {
+  return arr.sort((convo1, convoNext) => {
+    return (
+      new Date(convoNext?.latestMessage?.createdAt) -
+      new Date(convo1?.latestMessage?.createdAt)
+    );
+  });
+};
