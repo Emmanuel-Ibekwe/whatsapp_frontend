e8a0bb08880305a9ac8d279ee29cf6c00cc61cc6
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-03-09 14:19:33 +0100

    feat: created the getConversationPicture, getConversationName functions

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -10,3 +10,11 @@ export const sortInDescendingLatestMessageTime = arr => {
     );
   });
 };
+
+export const getConversationName = (user, users) => {
+  return users[0]._id === user._id ? users[1].name : users[0].name;
+};
+
+export const getConversationPicture = (user, users) => {
+  return users[0]._id === user._id ? users[1].picture : users[0].picture;
+};
