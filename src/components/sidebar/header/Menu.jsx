b41a74161a6cecad58285ebe8775dd212bcc6a64
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-03-16 11:35:48 +0100

    fix: Added socket.disconnect() to the logoutHandler to enable the socket to be disconnected when the user logs out

--- src/components/sidebar/header/Menu.jsx
+++ src/components/sidebar/header/Menu.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
 import { useDispatch } from "react-redux";
-
+import SocketContext from "../../../context/SocketContext";
 import { logout } from "../../../store/userSlice";
 
 export default function Menu() {
+  const { socket } = useContext(SocketContext);
+
   const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+    socket.disconnect();
+  };
+
   return (
     <div className="absolute right-1 z-50 dark:bg-dark_bg_4 dark:text-dark_text_1 shadow-md w-64 rounded-sm text-left border-b border-b-dark_bg_2">
       <ul>
@@ -24,7 +32,7 @@ export default function Menu() {
         </li>
         <li
           className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
-          onClick={() => dispatch(logout())}
+          onClick={() => logoutHandler()}
         >
           <span>Log out</span>
         </li>
