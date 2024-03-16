0fd7ffd040695402ef7f0e8c06de0ea265cf77a0
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-03-16 11:27:06 +0100

    feat: Added the SocketContext Provider to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
+import { io } from "socket.io-client";
 import { useEffect } from "react";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -11,13 +12,18 @@ import Login from "./pages/Login";
 import { logout } from "./store/userSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { getTokenDuration } from "./utils/auth";
+import SocketContext from "./context/SocketContext";
+import { sendMessage } from "./store/chatSlice";
 
+const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
+
+const ctxValue = {
+  socket
+};
 function App() {
   const dispatch = useDispatch();
   const { user } = useSelector(state => state.user);
   const { token } = user;
-  console.log(token);
-  console.log(user);
 
   useEffect(() => {
     const duration = getTokenDuration();
@@ -28,19 +34,21 @@ function App() {
   }, []);
 
   return (
-    <div className="dark">
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={token ? <Home /> : <Navigate to="/login" />}
-          />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </div>
+    <SocketContext.Provider value={ctxValue}>
+      <div className="dark">
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={token ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </div>
+    </SocketContext.Provider>
   );
 }
 
