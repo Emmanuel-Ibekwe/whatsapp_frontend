cf90008fc67d116c450a2fc2477a5ca769e72b23
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-04-17 13:15:10 +0100

    chore: Passed some additional props to the CallTimer component

--- src/components/Chat/call/CallArea.jsx
+++ src/components/Chat/call/CallArea.jsx
@@ -1,12 +1,26 @@
-export default function CallArea({ name }) {
+import CallTimer from "./CallTimer";
+
+export default function CallArea({
+  name,
+  totalSecsInCall,
+  setTotalSecsInCall,
+  callAccepted
+}) {
   return (
-    <div className="absolute top-12 w-full p-1">
+    <div className="absolute top-12 w-full p-1 z-40">
       <div className="flex flex-col items-center">
         <div className="flex flex-col items-center gap-y-1">
           <h1 className="text-white text-lg">
             <b>{name}</b>
           </h1>
-          <span className="text-dark_text_1">Ringing...</span>
+          {totalSecsInCall === 0 && (
+            <span className="text-dark_text_1">Ringing...</span>
+          )}
+          <CallTimer
+            totalSecsInCall={totalSecsInCall}
+            setTotalSecsInCall={setTotalSecsInCall}
+            callAccepted={callAccepted}
+          />
         </div>
       </div>
     </div>
