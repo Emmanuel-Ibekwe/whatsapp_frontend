0212eec5b598d85795281edb6dd56a323a511b43
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-04-19 11:57:26 +0100

    feat: Added some props to the Menu component and added the CreateGroup component

--- src/components/sidebar/header/SidebarHeader.js
+++ src/components/sidebar/header/SidebarHeader.js
@@ -2,49 +2,63 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 import { CommunityIcon, StoryIcon, ChatIcon, DotsIcon } from "../../../svg";
 import Menu from "./Menu";
+import CreateGroup from "./createGroup/CreateGroup";
 
 function SidebarHeader() {
   const { user } = useSelector(state => state.user);
   const [showMenu, setShowMenu] = useState(false);
+  const [showCreateGroup, setShowCreateGroup] = useState(false);
 
   return (
-    <div className="h-[60px] dark:bg-dark_bg_2 flex items-center p16">
-      <div className="w-full flex items-center justify-between ">
-        <button className="btn">
-          <img
-            src={user.picture}
-            alt="profile pic"
-            className="w-full h-full rounded-full object-cover"
-          />
-        </button>
-        <ul className="flex items-center gap-x-2.5">
-          <li>
-            <button className="btn">
-              <CommunityIcon className="dark:fill-dark_svg_1" />
-            </button>
-          </li>
-          <li>
-            <button className="btn">
-              <StoryIcon className="dark:fill-dark_svg_1" />
-            </button>
-          </li>
-          <li>
-            <button className="btn">
-              <ChatIcon className="dark:fill-dark_svg_1" />
-            </button>
-          </li>
-          <li className="relative">
-            <button
-              className={`btn ${showMenu ? "bg-dark_hover_1" : ""}`}
-              onClick={() => setShowMenu(prev => !prev)}
-            >
-              <DotsIcon className="dark:fill-dark_svg_1" />
-            </button>
-            {showMenu && <Menu />}
-          </li>
-        </ul>
+    <>
+      {/* SideBar header */}
+      <div className="h-[60px] dark:bg-dark_bg_2 flex items-center p16">
+        <div className="w-full flex items-center justify-between ">
+          <button className="btn">
+            <img
+              src={user.picture}
+              alt="profile pic"
+              className="w-full h-full rounded-full object-cover"
+            />
+          </button>
+          <ul className="flex items-center gap-x-2.5">
+            <li>
+              <button className="btn">
+                <CommunityIcon className="dark:fill-dark_svg_1" />
+              </button>
+            </li>
+            <li>
+              <button className="btn">
+                <StoryIcon className="dark:fill-dark_svg_1" />
+              </button>
+            </li>
+            <li>
+              <button className="btn">
+                <ChatIcon className="dark:fill-dark_svg_1" />
+              </button>
+            </li>
+            <li className="relative">
+              <button
+                className={`btn ${showMenu ? "bg-dark_hover_1" : ""}`}
+                onClick={() => setShowMenu(prev => !prev)}
+              >
+                <DotsIcon className="dark:fill-dark_svg_1" />
+              </button>
+              {showMenu && (
+                <Menu
+                  setShowCreateGroup={setShowCreateGroup}
+                  setShowMenu={setShowMenu}
+                />
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      {/* Create Group */}
+      {showCreateGroup && (
+        <CreateGroup setShowCreateGroup={setShowCreateGroup} />
+      )}
+    </>
   );
 }
 
