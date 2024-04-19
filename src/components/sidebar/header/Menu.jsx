3155967fd24c497efd285d6463a641c5e68957cf
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-04-19 11:56:14 +0100

    feat: Added an onclick function to set values of the showCreateGroup and showMenu state

--- src/components/sidebar/header/Menu.jsx
+++ src/components/sidebar/header/Menu.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import SocketContext from "../../../context/SocketContext";
 import { logout } from "../../../store/userSlice";
 
-export default function Menu() {
+export default function Menu({ setShowCreateGroup, setShowMenu }) {
   const { socket } = useContext(SocketContext);
 
   const dispatch = useDispatch();
@@ -15,7 +15,13 @@ export default function Menu() {
   return (
     <div className="absolute right-1 z-50 dark:bg-dark_bg_4 dark:text-dark_text_1 shadow-md w-64 rounded-sm text-left border-b border-b-dark_bg_2">
       <ul>
-        <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
+        <li
+          onClick={() => {
+            setShowCreateGroup(true);
+            setShowMenu(false);
+          }}
+          className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
+        >
           <span>New Group</span>
         </li>
         <li className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3">
